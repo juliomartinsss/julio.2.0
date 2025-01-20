8634b36af6ded0761dfc129eca26a7948050929f
juliomartinsss <160731695+juliomartinsss@users.noreply.github.com>
2025-01-20 10:34:10 -0300

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("A view funcionou")
+
+def sobre(request):
+    return HttpResponse("<h1> Sistema 1.0 desenvolvido por mim </h1>")
+
+def contato(request):
+    return HttpResponse("<h1> esta é a página de contato</h1>")
+
+def ajuda(request):
+    return HttpResponse("<h1>Esta é  a página de ajuda </h1>")
